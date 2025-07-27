02ac9ec38543a20fbab0bd635e2bd860a90f89cd
terry-u16 <terry-u16@users.noreply.github.com>
2025-07-27 17:34:46 +0900

    リファクタ

--- src/runner/list.rs
+++ src/runner/list.rs
@@ -2,7 +2,7 @@ use super::io::{load_result_json, AllResultJson};
 use crate::runner::io;
 use crate::runner::single::Objective;
 use crate::settings::Settings;
-use anyhow::Result;
+use anyhow::{ensure, Result};
 use colored::Colorize as _;
 use std::collections::HashMap;
 use std::fs;
@@ -30,19 +30,42 @@ struct ResultTableRow {
     comment: String,
 }
 
+/// 過去のテスト結果をリスト表示する関数
 pub(super) fn list_past_results(settings: &Settings, limit: Option<usize>) -> Result<()> {
+    // JSONファイルから結果を読み込む
+    let results = load_results(settings, limit)?;
+
+    // 絶対ベストスコア
+    let best_avg_absolute_score = calculate_best_avg_absolute_score(settings, &results);
+
+    // 相対ベストスコア
+    let best_scores = load_best_scores(settings);
+    let best_avg_relative_score =
+        calculate_best_avg_relative_score(settings, &results, &best_scores);
+
+    // テーブル形式で結果を表示
+    print_table(
+        settings,
+        results,
+        best_avg_absolute_score,
+        best_scores,
+        best_avg_relative_score,
+    );
+
+    Ok(())
+}
+
+fn load_results(settings: &Settings, limit: Option<usize>) -> Result<Vec<AllResultJson>> {
     let json_dir = io::get_json_dir_path(&settings.test.out_dir);
 
-    if !json_dir.exists() {
-        println!(
-            "No results found. JSON directory does not exist: {}",
-            json_dir.display()
-        );
-        return Ok(());
-    }
+    ensure!(
+        json_dir.exists(),
+        "No results found. JSON directory does not exist: {}",
+        json_dir.display()
+    );
 
-    // JSONファイルを検索
-    let mut json_files = Vec::new();
+    let mut json_files = vec![];
+
     for entry in fs::read_dir(&json_dir)? {
         let entry = entry?;
         let path = entry.path();
@@ -72,7 +95,10 @@ pub(super) fn list_past_results(settings: &Settings, limit: Option<usize>) -> Re
         .filter_map(|file| load_result_json(file).ok())
         .collect::<Vec<_>>();
 
-    // 絶対ベストスコア
+    Ok(results)
+}
+
+fn calculate_best_avg_absolute_score(settings: &Settings, results: &Vec<AllResultJson>) -> f64 {
     let best_avg_absolute_score = results
         .iter()
         .map(|result| {
@@ -88,18 +114,69 @@ pub(super) fn list_past_results(settings: &Settings, limit: Option<usize>) -> Re
         })
         .unwrap_or(f64::NAN);
 
-    // 相対ベストスコア
+    best_avg_absolute_score
+}
+
+fn load_best_scores(settings: &Settings) -> HashMap<u64, std::num::NonZeroU64> {
     let best_score_path = io::get_best_score_path(&settings.test.out_dir);
     let best_scores =
         io::load_best_scores(&best_score_path).unwrap_or_else(|_| std::collections::HashMap::new());
+
+    best_scores
+}
+
+fn calculate_best_avg_relative_score(
+    settings: &Settings,
+    results: &Vec<AllResultJson>,
+    best_scores: &HashMap<u64, std::num::NonZero<u64>>,
+) -> f64 {
     let best_avg_relative_score = results
         .iter()
-        .map(|result| calc_average_relative_score(result, &best_scores, settings.problem.objective))
+        .map(|result| calc_average_relative_score(result, best_scores, settings.problem.objective))
         .max_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal))
         .unwrap_or(f64::NAN);
 
+    best_avg_relative_score
+}
+
+fn calc_average_relative_score(
+    result: &AllResultJson,
+    best_scores: &HashMap<u64, NonZeroU64>,
+    objective: Objective,
+) -> f64 {
+    if result.case_count == 0 {
+        return 0.0;
+    }
+
+    let mut total_relative_score = 0.0;
+
+    for case in &result.cases {
+        if case.score == 0 {
+            continue; // スコアが0のケースは無視
+        }
+
+        let relative_score = match (best_scores.get(&case.seed).copied(), objective) {
+            (Some(best), Objective::Max) => case.score as f64 / best.get() as f64 * 100.0,
+            (Some(best), Objective::Min) => best.get() as f64 / case.score as f64 * 100.0,
+            (None, _) => 100.0,
+        };
+
+        total_relative_score += relative_score;
+    }
+
+    total_relative_score / result.case_count as f64
+}
+
+fn print_table(
+    settings: &Settings,
+    results: Vec<AllResultJson>,
+    best_avg_absolute_score: f64,
+    best_scores: HashMap<u64, std::num::NonZero<u64>>,
+    best_avg_relative_score: f64,
+) {
     // 結果を読み込んで表示
-    let mut table_rows = Vec::new();
+    let mut table_rows = vec![];
+
     for result in results {
         table_rows.push(convert_to_table_row(
             result,
@@ -115,8 +192,6 @@ pub(super) fn list_past_results(settings: &Settings, limit: Option<usize>) -> Re
     table.with(Style::markdown());
     table.modify(Columns::new(1..=4), Alignment::right());
     println!("{table}");
-
-    Ok(())
 }
 
 fn convert_to_table_row(
@@ -170,32 +245,4 @@ fn convert_to_table_row(
         tag: tag_display,
         comment: result.comment,
     }
-}
-
-fn calc_average_relative_score(
-    result: &AllResultJson,
-    best_scores: &HashMap<u64, NonZeroU64>,
-    objective: Objective,
-) -> f64 {
-    if result.case_count == 0 {
-        return 0.0;
-    }
-
-    let mut total_relative_score = 0.0;
-
-    for case in &result.cases {
-        if case.score == 0 {
-            continue; // スコアが0のケースは無視
-        }
-
-        let relative_score = match (best_scores.get(&case.seed).copied(), objective) {
-            (Some(best), Objective::Max) => case.score as f64 / best.get() as f64 * 100.0,
-            (Some(best), Objective::Min) => best.get() as f64 / case.score as f64 * 100.0,
-            (None, _) => 100.0,
-        };
-
-        total_relative_score += relative_score;
-    }
-
-    total_relative_score / result.case_count as f64
 }
